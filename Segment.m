da782401eb4fab3a314ba062479648f059de33a4
E0053462 <e0053462@u.nus.edu>
2019-11-20 11:07:19 +0800

    Update Segment.m

--- Segment.m
+++ Segment.m
@@ -1,5 +1,12 @@
 function [Image_Segments] = Segment(Labels,imagex,Min_size)
 % Segment a grayscale image using the binary image
+% Input: Labels - matrix of labels after component labelling
+%        imagex - a grayscale image from which the segments are extracted
+%        min_size - a minimum size for the segment (denoising)
+% Output: Image_Segments - a cell of matrices, each matrix representing a segmented character
+
+
+
 MAXI = max(Labels(:));
 MINI = 1; % All labels start from 1
 count = 1;
@@ -9,12 +16,12 @@ Full_Image_Segments = zeros(size(Labels));
 
 for kk = MINI:MAXI
     I = (Labels == kk); Iint = int8(I);
-    Full_Image_Segments(:,:,count) = Iint.*imagex;
+    Full_Image_Segments(:,:,count) = Iint.*imagex; % Multiply the logical array with grayscale to extract the character
     count = count + 1; 
 end
 
 
-% Denoising to remove noise segments
+% Denoising to remove noise segments: commented out as component labelling already removes noisy segments
 
 % count = 0;
 % for kk = 1:size(Full_Image_Segments,3)
@@ -30,9 +37,9 @@ end
 %     end
 % end
 
-Image_Segments = cell(1,size(Full_Image_Segments,3));
+Image_Segments = cell(1,size(Full_Image_Segments,3));  
 
-% Cropping
+% Cropping: crop segments to fit the character and remove zero padding
 for kk = 1:size(Full_Image_Segments,3)
     Image_Segments{kk} = Crops(Full_Image_Segments(:,:,kk));
 end
